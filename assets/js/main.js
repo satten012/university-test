520a349fb60f99bbb5cc87916f89d4b3b2d662fd
unknown <essatenilas@gmail.com>
2023-11-03 07:10:29 +0200

    add: footer, slider, main content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,32 @@
 var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
+    speed: 900,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
-    mousewheel: true,
-    keyboard: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+/*     mousewheel: true,
+    keyboard: true, */
+  });
+
+  var swiper = new Swiper(".mySwiper-2", {
+    speed: 700,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
   });
